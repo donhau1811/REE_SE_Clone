ed719f1badd77cdab732665da454dee165504e00
nguyenquanghuy-06 <62058996+nguyenquanghuy-06@users.noreply.github.com>
2022-07-14 14:57:18 +0700

    Feature/SP2D-55

--- src/views/billing/customer/ProjectTable.js
+++ src/views/billing/customer/ProjectTable.js
@@ -1,25 +1,40 @@
 import React from 'react'
-import { Col, Row, UncontrolledTooltip } from 'reactstrap'
-
-import { injectIntl, FormattedMessage } from 'react-intl'
+import { Col, Row } from 'reactstrap'
+import { injectIntl } from 'react-intl'
 import { object } from 'prop-types'
 import Table from '@src/views/common/table/CustomDataTable'
 import { billElectricArray } from './mock-data'
+import './scss/Filter.scss'
+import { Tooltip } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  customWidth: {
+    maxWidth: 500,
+    fontSize:16,
+    background:'#ffffff',
+    borderRadius:'3px',
+    color:"#486077"
+  }
+}))
 
 const ProjectTable = ({ intl }) => {
+  const classes = useStyles()
+
   const columns = [
     {
       name: intl.formatMessage({ id: 'No.' }),
       sortable: true,
       cell: (row, index) => index + 1,
       center: true,
-      maxWidth: '50px'
+      maxWidth: '40px',
+      style: { fontSize: '18px' }
     },
     {
       name: intl.formatMessage({ id: 'projectCode' }),
       selector: 'projectCode',
       sortable: true,
-      maxWidth: '120px'
+      maxWidth: '72px'
     },
     {
       name: intl.formatMessage({ id: 'projectName' }),
@@ -37,13 +52,14 @@ const ProjectTable = ({ intl }) => {
         return (
           <>
             <div id={`view_name${row.id}`}>
-              {row?.address?.length > 150 ? `${row.address.slice(0, 150)}...` : row.address}
+              {row?.address?.length > 150 ? (
+                <Tooltip classes={{ tooltip: classes.customWidth }} title={row.address}>
+                  <div className="text">{row.address.slice(0, 150)}...</div>
+                </Tooltip>
+              ) : (
+                row.address
+              )}
             </div>
-            {row?.address?.length > 150 && (
-              <UncontrolledTooltip placement="auto" target={`view_name${row.id}`}>
-                <FormattedMessage id={row.address} />
-              </UncontrolledTooltip>
-            )}
           </>
         )
       }
