3b139da99e7100f54378dbea626280f12dc18056
haimn-seta <haiminh.nguyen@setacinq.vn>
2022-08-19 15:53:18 +0700

    confirm in case clicking to breadcrums

--- src/views/billing/contract/PowerSelling/create.js
+++ src/views/billing/contract/PowerSelling/create.js
@@ -8,10 +8,12 @@ import SweetAlert from 'sweetalert2'
 import classNames from 'classnames'
 import '@src/@core/scss/billing-sweet-alert.scss'
 import withReactContent from 'sweetalert2-react-content'
-import { injectIntl } from 'react-intl'
+import { injectIntl, useIntl } from 'react-intl'
 import { object } from 'prop-types'
+import BreadCrumbs from '@src/views/common/breadcrumbs'
 
 const MySweetAlert = withReactContent(SweetAlert)
+
 function PowerSellingUpdateCOM({ intl }) {
   const history = useHistory()
   const dispatch = useDispatch()
@@ -86,3 +88,57 @@ PowerSellingUpdateCOM.propTypes = {
 }
 
 export default injectIntl(PowerSellingUpdateCOM)
+
+export const Navbar = () => {
+  const {
+    layout: { skin },
+    form: { isFormGlobalDirty }
+  } = useSelector((state) => state)
+  const intl = useIntl()
+  const history = useHistory()
+
+  const handleBreadCrumbsRedirct = (event) => {
+    event.preventDefault()
+    if (isFormGlobalDirty) {
+      return MySweetAlert.fire({
+        title: intl.formatMessage({ id: 'Cancel confirmation' }),
+        text: intl.formatMessage({ id: 'Are you sure to cancel?' }),
+        showCancelButton: true,
+        confirmButtonText: intl.formatMessage({ id: 'Yes' }),
+        cancelButtonText: intl.formatMessage({ id: 'No, Thanks' }),
+        customClass: {
+          popup: classNames({
+            'sweet-alert-popup--dark': skin === 'dark',
+            'sweet-popup': true
+          }),
+          header: 'sweet-title',
+          confirmButton: 'btn btn-primary',
+          cancelButton: 'btn btn-secondary ml-1',
+          actions: 'sweet-actions',
+          content: 'sweet-content'
+        },
+        buttonsStyling: false
+      }).then(({ isConfirmed }) => {
+        if (isConfirmed) {
+          history.push(ROUTER_URL.BILLING_PROJECT)
+        }
+      })
+    }
+    history.push(ROUTER_URL.BILLING_PROJECT)
+  }
+
+  const tempItems = [
+    { name: intl.formatMessage({ id: 'billing' }), link: '' },
+    { name: intl.formatMessage({ id: 'project management' }), link: '' },
+    {
+      name: intl.formatMessage({ id: 'project' }),
+      innerProps: { tag: 'a', href: '#', onClick: handleBreadCrumbsRedirct }
+    },
+    { name: intl.formatMessage({ id: 'power-selling-contract' }), link: '' }
+  ]
+  return (
+    <>
+      <BreadCrumbs breadCrumbItems={tempItems} />
+    </>
+  )
+}
